ddd83a696e8e28ac50dc9384610bf2ddac05e2df
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-12-06 11:36:32 +0200

    Add details page

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,15 +1,19 @@
 const { Router } = require('express');
 const router = Router();
 
-
+//actions
 router.get('/', (req, res) => {
-    //action
     res.render('home.hbs', {title: 'Browse'})
 });
 
 router.get('/create', (req, res) => {
     res.render('create.hbs', {title: 'Create'})
 });
+
+router.get('/details/:productId', (req, res) => {
+    console.log(req.params.productId);
+    res.render('details.hbs', {title: 'Details'})
+})
 
 module.exports = router;
 
