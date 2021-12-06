3524ad95f83470000724fa243fe4c0dfd78bca27
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-12-06 11:17:47 +0200

    Change about home controller

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,6 +2,10 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
+    res.redirect('/products')
+})
+
+router.get('/about', (req, res) => {
     //action
     res.render('about.hbs', {title: 'About'})
 });
